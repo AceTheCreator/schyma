4b131f78f7c6730bc293bdbf45f1163c30e403d8
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-11-01 15:20:26 +0530

    removed some useless formattings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react'
-import { Edge, Node, Position } from 'reactflow'
-import Panel from './Panel'
-import { useState } from 'react'
-import Nodes from './Nodes'
-import { JSONSchema7Object } from 'json-schema'
-import Ajv from 'ajv'
-import { propMerge } from '../utils/reusables'
-import dagre from 'dagre'
-import { nodeHeight, nodeWidth } from '../constants/node'
+import React, { useEffect } from 'react';
+import { Edge, Node, Position } from 'reactflow';
+import Panel from './Panel';
+import { useState } from 'react';
+import Nodes from './Nodes';
+import { JSONSchema7Object } from 'json-schema';
+import Ajv from 'ajv';
+import { propMerge } from '../utils/reusables';
+import dagre from 'dagre';
+import { nodeHeight, nodeWidth } from '../constants/node';
 
 interface Default {
-  title: string
-  description: string
+  title: string;
+  description: string;
   schema: JSONSchema7Object
 }
+
 
 function Schyma({ title, description, schema }: Default) {
   const ajv = new Ajv();
@@ -32,7 +33,7 @@ function Schyma({ title, description, schema }: Default) {
     },
     position,
   }
-  const validate = ajv.validateSchema(schema)
+  const validate = ajv.validateSchema(schema);
 
   const dagreGraph = new dagre.graphlib.Graph()
 
@@ -70,7 +71,7 @@ function Schyma({ title, description, schema }: Default) {
     if(validate){
       setRender(true)
     }
-  }, [validate])
+  },[validate])
   return (
     <div>
       {render ? (
